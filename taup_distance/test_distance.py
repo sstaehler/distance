1312919eced4b2806bfc4e5cab15e284cf928956
Simon Stähler <simon.staehler@erdw.ethz.ch>
2020-12-19 13:48:42 +0100

    Better test

--- taup_distance/test_distance.py
+++ taup_distance/test_distance.py
@@ -9,7 +9,23 @@
 """
 from taup_distance.taup_distance import get_dist
 from obspy.taup import TauPyModel
-model = TauPyModel('iasp91')
+import numpy as np
 
-dist = get_dist(model=model, tSmP=400, depth=50.)
+model = TauPyModel('iasp91')
+tSmP = 400.
+dist = get_dist(model=model, tSmP=tSmP, depth=50.)
+arrivals = model.get_travel_times(source_depth_in_km=50., distance_in_degree=dist,
+                       phase_list=('P', 'S'))
+has_P = False
+has_S = False
+for arr in arrivals:
+    if arr.name == 'P' and not has_P:
+        tP = arr.time
+        has_P = True
+    if arr.name == 'S' and not has_S:
+        tS = arr.time
+        has_S = True
+
+np.testing.assert_almost_equal(actual = tS - tP, desired = tSmP, decimal=3)
+
 print(dist)
